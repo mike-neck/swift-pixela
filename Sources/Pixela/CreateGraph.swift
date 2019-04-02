5882947540076864d61d4dbc7034e7b7b0fb5340
mike_neck <jkrt3333@gmail.com>
2019-04-02 21:24:05 +0900

    add create-graph request

--- Sources/Pixela/CreateGraph.swift
+++ Sources/Pixela/CreateGraph.swift
@@ -7,7 +7,7 @@ struct CreateGraph: Codable, CustomStringConvertible {
     let unit: String
     let type: String
     let color: String
-    let timezone: String?
+    let timezone: String
     let selfSufficient: String?
 
     private init(id: String,
@@ -15,7 +15,7 @@ struct CreateGraph: Codable, CustomStringConvertible {
                  unit: String,
                  typeAsString: String,
                  colorAsString: String,
-                 timezone: String?,
+                 timezone: String,
                  selfSufficientAsString: String?) {
         self.id = id
         self.name = name
@@ -31,8 +31,8 @@ struct CreateGraph: Codable, CustomStringConvertible {
          unit: String,
          type: Type,
          color: Color,
-         timezone: String?,
-         selfSufficient: Diff?) {
+         timezone: String,
+         selfSufficient: DiffType?) {
         self.init(
                 id: id,
                 name: name,
@@ -44,7 +44,6 @@ struct CreateGraph: Codable, CustomStringConvertible {
     }
 
     var description: String {
-        let tz = timezone ?? ""
         let sf = selfSufficient ?? ""
         return """
                id = \(id),
@@ -52,13 +51,52 @@ struct CreateGraph: Codable, CustomStringConvertible {
                unit = \(unit),
                type = \(type),
                color = \(color),
-               timezone = \(tz),
+               timezone = \(timezone),
                selfSufficient = \(sf),
                """
     }
 }
 
-enum Type {
+struct CreateGraphRequest: Request {
+
+    typealias RESPONSE = PixelaResponse
+    typealias BODY = CreateGraph
+
+    private let pixela: Pixela
+    private let createGraph: CreateGraph
+
+    init(pixela: Pixela, createGraph: CreateGraph) {
+        self.pixela = pixela
+        self.createGraph = createGraph
+    }
+
+    var path: String {
+        return "/v1/users/\(pixela.username)/graphs"
+    }
+
+    let httpMethod: HttpMethod = .post
+
+    var userToken: String? {
+        return pixela.token
+    }
+
+    func body() -> CreateGraph? {
+        return createGraph
+    }
+
+    func responseType() -> PixelaResponse.Type {
+        return PixelaResponse.self
+    }
+
+    var description: String {
+        return """
+               POST \(path)
+               \(createGraph)
+               """
+    }
+}
+
+public enum Type {
     case int
     case float
 
@@ -70,7 +108,7 @@ enum Type {
     }
 }
 
-enum Color {
+public enum Color {
     case shibafu
     case momiji
     case sora
@@ -90,7 +128,7 @@ enum Color {
     }
 }
 
-enum Diff {
+public enum DiffType {
     case increment
     case decrement
 
--- Sources/Pixela/Pixela.swift
+++ Sources/Pixela/Pixela.swift
@@ -5,7 +5,7 @@ public struct Pixela: CustomStringConvertible {
     let username: String
     let token: String
 
-    private let httpClient: HttpClient
+    fileprivate let httpClient: HttpClient
 
     init(username: String, token: String, httpClient: HttpClient) {
         self.username = username
@@ -35,13 +35,58 @@ public struct Pixela: CustomStringConvertible {
         let request = DeleteUserRequest(pixela: self)
         let queue = httpClient.queue
         return httpClient.sendRequest(request)
-        .then(on: queue) { (response: PixelaResponse) throws -> Promise<Void> in
-            return Promise(on: queue) { () -> Void in
-                guard true == response.isSuccess else {
-                    throw PixelaApiError.invalidResponse(message: "error - \(response.message)")
+                .then(on: queue) { (response: PixelaResponse) throws -> Promise<Void> in
+                    return Promise(on: queue) { () -> Void in
+                        guard true == response.isSuccess else {
+                            throw PixelaApiError.invalidResponse(message: "error - \(response.message)")
+                        }
+                        return ()
+                    }
                 }
-                return ()
-            }
-        }
+    }
+
+    func createGraph(id: String,
+                     name: String,
+                     unit: String,
+                     type: Type,
+                     color: Color,
+                     timezone: String = "UTC",
+                     selfSufficient: DiffType? = nil) -> Promise<Graph> {
+        let request = CreateGraphRequest(
+                pixela: self,
+                createGraph: CreateGraph(
+                        id: id,
+                        name: name,
+                        unit: unit,
+                        type: type,
+                        color: color,
+                        timezone: timezone,
+                        selfSufficient: selfSufficient))
+        let queue = httpClient.queue
+        let px = self
+        return httpClient.sendRequest(request)
+                .then(on: queue) { (response: PixelaResponse) throws -> Promise<Graph> in
+                    return Promise(on: queue) { () -> Graph in
+                        guard  true == response.isSuccess else {
+                            throw PixelaApiError.invalidResponse(message: "error - \(response.message)")
+                        }
+                        return Graph(pixela: px, graphId: id)
+                    }
+                }
+    }
+}
+
+public struct Graph: CustomStringConvertible {
+
+    private let pixela: Pixela
+    private let graphId: String
+
+    public init(pixela: Pixela, graphId: String) {
+        self.pixela = pixela
+        self.graphId = graphId
+    }
+
+    public var description: String {
+        return "graph[username:\(pixela.username),graph:\(graphId)]"
     }
 }

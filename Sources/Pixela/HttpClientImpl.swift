e1f45e76d099be4d3cc7019fdc02875db82cadc0
mike_neck <jkrt3333@gmail.com>
2019-04-06 23:09:34 +0900

    use CompletionHandler in completionHandler

--- Sources/Pixela/HttpClientImpl.swift
+++ Sources/Pixela/HttpClientImpl.swift
@@ -50,45 +50,13 @@ class HttpClientImpl: HttpClient {
             }
 
             let task = self.urlSession.dataTask(with: req, completionHandler: { (data: Data?, resp: URLResponse?, err: Error?) in
-                if let error = err {
-                    reject(PixelaApiError.unexpected(error: error))
-                    return
-                }
-                guard let response = resp as? HTTPURLResponse else {
-                    let description = String(describing: resp)
-                    reject(PixelaApiError.invalidResponse(message: "error - unknown type response \(description)"))
-                    return
-                }
-
-                let statusCode = response.statusCode
-                guard 200 <= statusCode && statusCode < 300 else {
-                    guard let body = data else {
-                        reject(PixelaApiError.invalidResponse(message: "error - status: \(statusCode) without body"))
-                        return
-                    }
-                    do {
-                        let pixelaResponse = try self.decoder.decode(PixelaResponse.self, from: body)
-                        reject(PixelaApiError.apiError(response: pixelaResponse))
-                        return
-                    } catch {
-                        let json = String(data: body, encoding: .utf8) ?? "[decoding error]"
-                        reject(PixelaApiError.invalidResponse(message: "error - status: \(statusCode) with body: \(json)"))
-                        return
-                    }
-                }
-
-                guard let body = data else {
-                    reject(PixelaApiError.invalidResponse(message: "error - api is success without response body"))
-                    return
-                }
-
-                do {
-                    let pixelaResponse = try self.decoder.decode(RES.self, from: body)
-                    success(pixelaResponse)
-                } catch {
-                    let json = String(data: body, encoding: .utf8) ?? "[decoding error]"
-                    reject(PixelaApiError.invalidResponse(message: "error - api is success but decoding response failed: \(json)"))
-                }
+                let completionHandler = CompletionHandler(decoder: self.decoder, data: data, resp: resp, err: err)
+                let result: Result<RES, Error> = completionHandler.handle()
+                result.doOn(success: { (res: RES) -> Void in
+                    success(res)
+                }, failure: { (err: Error) -> Void in
+                    reject(err)
+                })
             })
             task.resume()
         }
@@ -189,9 +157,8 @@ struct CompletionHandler {
         self.err = err
     }
 
-    func handle<Value: Codable>() -> Result<Value, Error> {
-        return handleResponseError(err).flatMap {
-            (Void) -> Result<Int, Error> in
+    func handle<Value: Decodable>() -> Result<Value, Error> {
+        return handleResponseError(err).flatMap { (Void) -> Result<Int, Error> in
             status.handle().mapError {
                 $0.handleIfAvailable(data: self.data, using: self.decoder)
             }
